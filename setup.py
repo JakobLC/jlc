82c0f7396dfb7344ff7ac2473acf7ef76371a623
JakobLC <jakoblc@live.dk>
2023-04-16 22:55:06 +0200

    package req added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jakob Loenborg Christensen',
     author_email='jakoblc@live.dk',
     description="Useful functions.",
-    version="1.2",
+    version="1.3",
     packages=find_packages(),
     install_requires=["tifffile",
                       "numpy",
