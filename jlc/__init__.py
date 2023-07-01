a7ab4f13ef4d7167610f725ab50997645041c0d6
jakob_git <jakoblc@live.dk>
2023-07-01 15:00:10 +0200

    added emd2

--- jlc/__init__.py
+++ jlc/__init__.py
@@ -1,2 +1,4 @@
-from .functions import montage,cat,reverse_dict,num_of_params,collage,standardize_image_like
+from .functions import montage,cat,reverse_dict,num_of_params,collage,standardize_image_like,earth_mover_distance
 from .voltools import inspect_vol,inspect_tifvol,load_tifvol,save_tifvol
+import functions
+import voltools
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jakob Loenborg Christensen',
     author_email='jakoblc@live.dk',
     description="Useful functions.",
-    version="1.5.8",
+    version="1.5.9",
     packages=find_packages(),
     install_requires=["tifffile",
                       "numpy",
