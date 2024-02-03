073e34360746ed1f3fb993e0519812951e102dfa
Jakob Lønborg Christensen <jloch@dtu.dk>
2024-02-03 13:46:43 +0100

    pretty point added, text rendering added 2

--- jlc/__init__.py
+++ jlc/__init__.py
@@ -1,6 +1,7 @@
 from .functions import (montage,cat,reverse_dict,num_of_params,collage,
                         standardize_image_like,earth_mover_distance,
-                        montage_save,DataloaderIterator,zoom)
+                        montage_save,DataloaderIterator,zoom,pretty_point,
+                        render_text,get_bbox_params)
 from .voltools import inspect_vol,inspect_tifvol,load_tifvol,save_tifvol
 from . import functions
 from . import voltools
